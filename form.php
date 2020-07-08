26706b8f66c8e4833ee99f1c198a6564f1b90837
FaizalDananjaya <62741758+FaizalDananjaya@users.noreply.github.com>
2020-07-08 19:50:36 +0700

    Add files via upload

---
+++ form.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>forum</title>
+</head>
+<body>
+        <p>
+            <label>Nama:</label>
+            <input type="text" name="nama"/>
+        </p>
+        <p>
+            <label>Alamat:</label>
+            <input type="text" name="Alamat"/>
+        </p>
+        <p>
+            <label>telp. (jangan telepon pribadi nanti ada sesuatu hehehe):</label>
+            <input type="text" name="telp"> 
+        </p>
+        <p>
+            <label>Jenis kelamin:</label>
+            <label><input type="radio" name="jenis_kelamin" value="laki-laki" /> Laki-laki</label>
+            <label><input type="radio" name="jenis_kelamin" value="perempuan" /> Perempuan</label>
+        </p>
+        <p>
+            <label>Agama:</label>
+            <select name="agama">
+                <option value="konghuchu">konghuchu</option>
+                <option value="islam">Islam</option>
+                <option value="kristen">Kristen</option>
+                <option value="hindu">Hindu</option>
+                <option value="budha">Budha</option>
+            </select>
+        </p>
+        <p>
+            <label>keanehan pada diri anda:</label>
+            <textarea name="keanehan"></textarea>
+        </p>
+        <p>
+            <input type="submit" name="submit" value="Daftar" />
+        </p>
+        <a href="tabel.html" target="blank">tentang saya</a>
+    </form>
+</body>
+</html>
